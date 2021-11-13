063db93559889abcc10b97b3b17f136df1c57bf3
Ian Percy <ianedwardpercy@gmail.com>
2021-11-13 01:35:09 -0600

    Player, Ground, Animation Start
    Put in the player, put in the ground with colliders, put in the player movement and physics system scripts, began on the player's animation controller.

---
+++ Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    [Header("Component References")]
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    
+    [Header("Animation")]
+    [SerializeField] string currentState;
+
+    [Header("Script References")]
+    [SerializeField] PhysicsObject physicsobject;
+
+    void Start(){
+
+        playerAnimator = this.gameObject.GetComponent<Animator>();
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+
+        physicsobject = this.gameObject.GetComponent<PhysicsObject>();
+    } 
+
+    void Update(){
+        if(Input.GetKey("a") || Input.GetKey("left"))
+                spriteRenderer.flipX = true;
+
+         if(Input.GetKey("d") || Input.GetKey("right"))
+                spriteRenderer.flipX = false;
+
+        if(!physicsobject.grounded){
+            Debug.Log("not grounded");
+        }
+    }
+
+    public void ChangeAnimatorState(string newState){
+        if(currentState == newState) return;
+
+        playerAnimator.Play(newState);
+
+        currentState = newState;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : PhysicsObject
+{
+    // Start is called before the first frame update
+        public float maxSpeed = 3.1f;
+        public float jumpTakeOffSpeed = 7f;
+        //PlayerAnimatorController playeranimatorcontroller;
+        public AudioSource jump;
+        public AudioSource big_jump;
+        //PauseMenu pausemenu;
+
+        void Start()
+        {
+            //playeranimatorcontroller = this.GetComponent<PlayerAnimatorController>();
+            //pausemenu = GameObject.Find("Pause").GetComponent<PauseMenu>();
+        }
+
+        void Awake()
+        {
+
+        }
+
+        protected override void ComputeVelocity()
+        {
+            Vector2 move = Vector2.zero;
+            
+            move.x = Input.GetAxis("Horizontal");
+            
+            
+            
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+                jump.Play();
+            }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * 0.5f;
+                }
+            }
+
+
+            targetVelocity = move * maxSpeed;
+        }
+}
